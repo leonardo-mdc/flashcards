064719660de5415735ec7d1af874d1b8db7145ed
Leonardo Carvalho <leonardomdc@outlook.com>
2026-06-03 12:31:31 -0300

    fix: use if0_41632431_flashcards database

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'db' => [
+        'host' => 'sql111.infinityfree.com',
+        'name' => 'if0_41632431_flashcards',
+        'user' => 'if0_41632431',
+        'pass' => '4p0LL0cr33d',
+    ],
+];
